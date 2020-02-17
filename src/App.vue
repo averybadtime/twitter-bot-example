6376001c5056a7c563cc224910b56861664562c4
Jean Carlos Ospino Gonzalez <devjc23@gmail.com>
2020-02-16 23:57:11 -0500

    Api integrada

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,176 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <navigation/>
+    <div class="card">
+      <div class="card_title">Esto no es un bot ~(ಠ︵ಠ)</div>
+      <div class="card_content">
+        <form v-on:submit.prevent="go">
+          <div class="input_group">
+            <label for="username">Twittear como:</label>
+            <select name="username"
+              id="username"
+              v-model="username">
+              <option :value="null">--- Selecciona un usuario ---</option>
+              <option v-for="user in accounts"
+                :key="user.username">
+                {{ user.username }}
+              </option>
+            </select>
+          </div>
+          <div class="input_group">
+            <label for="tweet">Contenido del tweet</label>
+            <textarea name="tweet"
+              id="tweet"
+              cols="30"
+              rows="10"
+              v-model="tweet">
+            </textarea>
+          </div>
+          <div class="input_group">
+            <label for="quantity">Número de réplicas</label>
+            <input type="number"
+              name="quantity"
+              id="quantity"
+              v-model.number="quantity">
+          </div>
+          <div class="input_group">
+            <button :disabled="!username">Envíar</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from "axios"
+  import Navigation from "./components/Navigation"
+  export default {
+    components: {
+      Navigation
+    },
+    data() {
+      return {
+        accounts: [],
+        tweet: "",
+        username: null,
+        quantity: 0
+      }
+    },
+    computed: {
+      selectedAccountConfig() {
+        if (this.username) {
+          const account = this.accounts.find(x => x.username == this.username)
+          const defaults = {
+            timeout_ms: 60 * 1000,
+            strictSSL : true
+          }
+          return Object.assign(defaults, account.config)
+        }
+        return null
+      }
+    },
+    methods: {
+      go() {
+        if (
+          this.username && this.username.trim() != "" &&
+          this.tweet && this.tweet.trim() != "" &&
+          this.quantity && this.quantity.toString().trim() != ""
+        ) {
+          if (this.quantity > 0) {
+            const payload = {
+              config: this.selectedAccountConfig,
+              tweet: {
+                content: this.tweet,
+                quantity: this.quantity
+              }
+            }
+            axios.post("http://localhost:3000/api/post", payload)
+              .then(({ data }) => {
+                this.username = null
+                this.tweet    = ""
+                this.quantity = 0
+                alert("Cadena propagada con éxito.")
+              })
+              .catch(error => alert("Ha ocurrido un error al ejecutar la operación."))
+          } else {
+            alert("El número de réplicas debe ser mayor a 0.")
+          }
+        } else {
+          alert("Asegúrate de rellenar todos los campos.")
+        }
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  @import url("https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap");
+  * {
+    box-sizing : border-box;
+    font-family: "Open Sans", sans-serif;
+  } 
+  body {
+    margin : 0;
+    padding: 0;
+  }
+  #app {
+    align-items    : center;
+    display        : flex;
+    height         : 100vh;
+    justify-content: center;
+    background     : radial-gradient(circle, #266a64, #1b1b25);
+    width          : 100vw;
+  }
+  .card {
+    color    : #ffffff;
+    max-width: 450px;
+    padding  : 30px 24px;
+    width    : 100%;
+  }
+  .card_title {
+    font-size  : 24px;
+    font-weight: bold;
+  }
+  .input_group {
+    margin-top: 18px;
+  }
+  .input_group label {
+    font-weight  : 700;
+    margin-bottom: 6px;
+    opacity      : .75;
+  }
+  .input_group * {
+    display: block;
+    outline: none;
+    width  : 100%;
+  }
+  .input_group *:not(label) {
+    -webkit-appearance: initial;
+    background-color  : rgba(255, 255, 255, .1);
+    border            : none;
+    border-radius     : 3px;
+    color             : #ffffff;
+    font-size         : 14px;
+    padding           : 6px 12px;
+  }
+  .input_group textarea {
+    resize: none;
+  }
+  .input_group button {
+    background-color: #266a64;
+    box-shadow: 1px 1px 12px 3px rgba(0, 0, 0, 0.13);
+    color: rgba(255, 255, 255, .75);
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: 700;
+    letter-spacing: 1px;
+    padding: 12px;
+  }
+  .input_group button:hover {
+    color: #ffffff;
+  }
+  .input_group button:disabled {
+    cursor: not-allowed;
+  }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,113 @@
+<template>
+  <nav class="sidebar">
+    <div class="menu_trigger">
+      <a href="javascript:void(0)">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+    <ul class="menu">
+      <li>
+        <a v-on:click.prevent="getAllAccounts">Todas las cuentas</a>
+      </li>
+      <li>
+        <a v-on:click.prevent="newAccount">Agregar nueva cuenta</a>
+      </li>
+      <li></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    methods: {
+      getAllAccounts() {
+        console.log("Get all accounts")
+      },
+      newAccount() {
+        console.log("Open new account modal")
+      },
+      navigation() {
+        const triggerButton = document.querySelector(".menu_trigger a")
+        const sidebar       = document.querySelector("nav.sidebar")
+        const menuLinks = document.querySelectorAll("nav .menu a")
+        menuLinks.forEach(link => {
+          link.addEventListener("click", ev => {
+            ev.preventDefault()
+            sidebar.classList.remove("open")
+          })
+        })
+        triggerButton.addEventListener("click", ev => {
+          ev.preventDefault()
+          sidebar.classList.contains("open")
+            ? sidebar.classList.remove("open")
+            : sidebar.classList.add("open")
+        })
+      }
+    },
+    mounted() {
+      this.navigation()
+    }
+  }
+</script>
+
+<style scoped>
+  nav.sidebar {
+    align-items    : center;
+    background     : #1b1b25;
+    color          : #ffffff;
+    display        : flex;
+    height         : 100vh;
+    justify-content: center;
+    left           : -100vw;
+    position       : absolute;
+    transition     : left .5s;
+    width          : 100vw;
+    z-index        : 999;
+  }
+  nav.sidebar.open {
+    left: 0px;
+  }
+  nav.sidebar.open .menu_trigger {
+    right: 20px;
+  }
+  nav.sidebar .menu_trigger {
+    align-items    : flex-end;
+    display        : flex;
+    flex-direction : column;
+    height         : 60px;
+    justify-content: center;
+    position       : absolute;
+    right          : -50px;
+    top            : 0px;
+    transition     : right .25s;
+  }
+  nav.sidebar .menu_trigger span {
+    background-color: rgba(255, 255, 255, .85);
+    display         : block;
+    height          : 2px;
+    margin-bottom   : 6px;
+    width           : 25px;
+  }
+  ul.menu {
+    list-style: none;
+    padding   : 0px;
+  }
+  ul.menu li {
+    margin-bottom: 24px;
+    text-transform: uppercase;
+  }
+  ul.menu li a {
+    color: rgba(255, 255, 255, .85);
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: .5px;
+    text-decoration: none;
+    transition: all .25s;
+  }
+  ul.menu li a:hover {
+    text-shadow: 1px 1px 13px rgba(255, 255, 255, .85);
+    text-decoration: underline;
+  }
+</style>
